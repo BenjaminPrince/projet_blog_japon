6194e6d1eefe5faf25b305461f867cc6bd9517f0
BenjaminPrince <prince-benjamin@hotmail.com>
2022-07-28 16:33:06 +0200

    Création du système de catégories

--- src/Controller/Admin/ArticleCrudController.php
+++ src/Controller/Admin/ArticleCrudController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\Article;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
@@ -27,6 +28,8 @@ class ArticleCrudController extends AbstractCrudController
         yield TextEditorField::new('content');
 
         yield TextField::new('featuredText');
+
+        yield AssociationField::new('categories');
         
         yield DateField::new('createdAt')
         ->hideOnForm();
