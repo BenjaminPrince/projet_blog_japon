1b62588604a6804ce8376ec27e37f3e4329d3fe4
BenjaminPrince <prince-benjamin@hotmail.com>
2022-08-01 15:50:58 +0200

    modif

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -37,12 +37,14 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToRoute('Retourner sur le site', 'fa fa-undo', 'app_home');
 
         yield MenuItem::subMenu('Articles', 'fas fa-newspaper')->setSubItems([
             MenuItem::linkToCrud('Tous les articles','fas fa-newspaper',Article::class),
             MenuItem::linkToCrud('Ajouter','fas fa-plus',Article::class)->setAction(Crud::PAGE_NEW),
             MenuItem::linkToCrud('Categories','fas fa-list',Category::class)
         ]);
+
+        yield MenuItem::linkToCrud('Commentaires', 'fas fa-comment', Comment::class);
     }
 }
