d21efcee2e65555088e4a8fa422e815aca97d8c7
BenjaminPrince <prince-benjamin@hotmail.com>
2022-11-15 14:09:51 +0100

    blog japon

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,20 +5,26 @@ namespace App\Controller;
 use App\Repository\ArticleRepository;
 use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(ArticleRepository $articleRepo,CategoryRepository $categoryRepository): Response
+    public function index(ArticleRepository $articleRepo,CategoryRepository $categoryRepository, Request $request): Response
     {
 
-      // $input = $request->query->get('search');
-      // $articles = $articleRepo->findBy(array(),array('created_at'=>'DESC'));
+      $articles = $articleRepo->findAll();
+      $articles =$articleRepo->findArticleOrderByDesc($articles);
+      // foreach ($articlesAll as $article) {
+      //   $artID = $article->getId();
+      //   array_push($articles, $article->findArticleOrderByDesc());
+        
+      // }
 
         return $this->render('home/index.html.twig', [
-          'articles' => $articleRepo->findAll(),
+          'articles' => $articles,
           'categories' => $categoryRepository->findAll()
         ]);
     }
